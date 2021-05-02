844fe5b226ea18f3188f16e9c0745549b8299d01
Dmitry Chebykin <dim_c@mail.ru>
2021-05-03 01:52:14 +0700

    fix

--- src/main/java/ArithmeticalAverage.java
+++ src/main/java/ArithmeticalAverage.java
@@ -2,66 +2,68 @@ import java.util.Scanner;
 
 public class ArithmeticalAverage {
     public static void main(String[] args) {
-        System.out.println("Введите диапазон - два целых числа через пробелы (например: -5  132 ):");
-
         Scanner scanner = new Scanner(System.in);
-        String inputLine = scanner.nextLine().trim();
 
-        String[] inputVariables = inputLine.trim().split("\\s+");
-        inputVariables[0] = inputVariables[0].trim();
-        inputVariables[1] = inputVariables[1].trim();
+        System.out.println("Введите начало диапазона -  целое число :");
+        int startNumber = scanner.nextInt();
 
-        int firstNumber = 0;
-        int secondNumber = 0;
+        System.out.println("Введите конец диапазона, второе целое число :");
+        int finishNumber = scanner.nextInt();
 
-        try {
-            firstNumber = Integer.parseInt(inputVariables[0]);
-            secondNumber = Integer.parseInt(inputVariables[1]);
-        } catch (Exception e) {
-            System.out.println("Неверный ввод, программа завершена.");
-            System.exit(0);
+        if (startNumber > finishNumber) { // fix incorrect input
+            int temp = finishNumber;
+            finishNumber = startNumber;
+            startNumber = temp;
         }
 
-        if (firstNumber > secondNumber) { //the smaller of the two entered numbers, put at the beginning of the range
-            firstNumber ^= secondNumber;
-            secondNumber ^= firstNumber;
-            firstNumber ^= secondNumber;
-        }
-
-        int countNumbers = (firstNumber == secondNumber) ? 2 : Math.abs(firstNumber - secondNumber) + 1;
-
-        int allEvenNumbersSum = 0;
-        int allOddNumbersSum = 0;
+        int evenNumbersSum = 0;
+        int oddNumbersSum;
         int allNumbersSum = 0;
+        int count = 0;
+        int evenCount = 1;
+        int temp = startNumber;
 
-        if (firstNumber == secondNumber) {
-            allNumbersSum = firstNumber + secondNumber;
-            allEvenNumbersSum = (firstNumber % 2 == 0) ? (firstNumber + secondNumber) : 0;
-            allOddNumbersSum = (firstNumber % 2 != 0) ? (firstNumber + secondNumber) : 0;
-        } else {
+        while (temp <= finishNumber) {
+            allNumbersSum = allNumbersSum + temp;
 
-            for (int i = 0; i <= countNumbers - 1; i++) {
-                allNumbersSum += i + firstNumber;
-
-                if ((firstNumber + i) % 2 != 0) {
-                    allOddNumbersSum += firstNumber + i;
-                } else {
-                    allEvenNumbersSum += firstNumber + i;
-                }
+            if (temp % 2 == 0) {
+                evenNumbersSum = evenNumbersSum + temp;
+                evenCount++;
             }
+            temp++;
+            count++;
         }
 
-        System.out.printf("Сумма всех целых чисел от %d до %d равна:%n", firstNumber, secondNumber);
+        if (allNumbersSum == finishNumber) {
+            allNumbersSum *= 2;
+            evenNumbersSum *= 2;
+            count *= 2;
+        } else {
+            evenCount -= 1;
+        }
+        oddNumbersSum = allNumbersSum - evenNumbersSum;
+
+        printInfo(startNumber, finishNumber, evenNumbersSum, oddNumbersSum,
+                allNumbersSum, count, evenCount);
+    }
+
+    static void printInfo(int startNumber, int finishNumber, int evenNumbersSum, int oddNumbersSum,
+                          int allNumbersSum, int count, int evenCount) {
+        System.out.printf("Сумма всех целых чисел от %d до %d равна:%n", startNumber, finishNumber);
         System.out.println(allNumbersSum);
 
-        System.out.printf("Сумма всех четных целых чисел от %d до %d равна:%n", firstNumber, secondNumber);
-        System.out.println(allEvenNumbersSum);
+        System.out.printf("Сумма всех четных целых чисел от %d до %d равна:%n", startNumber, finishNumber);
+        System.out.println(evenNumbersSum);
 
-        System.out.printf("Сумма всех нечетных целых чисел от %d до %d равна:%n", firstNumber, secondNumber);
-        System.out.println(allOddNumbersSum);
+        System.out.printf("Сумма всех нечетных целых чисел от %d до %d равна:%n", startNumber, finishNumber);
+        System.out.println(oddNumbersSum);
 
-        double allNumberArithmeticalAverageValue = (double) allNumbersSum / countNumbers;
-        System.out.printf("Среднее арифметическое всех целых чисел от %d до %d равно:%n", firstNumber, secondNumber);
-        System.out.println(allNumberArithmeticalAverageValue);
+        double allNumberAverage = (double) allNumbersSum / count;
+        System.out.printf("Среднее арифметическое всех целых чисел от %d до %d равно:%n", startNumber, finishNumber);
+        System.out.println(allNumberAverage);
+
+        double evenNumbersAverage = (evenCount == 0) ? 0 : (double) evenNumbersSum / evenCount;
+        System.out.printf("Среднее арифметическое всех четных чисел от %d до %d равно:%n", startNumber, finishNumber);
+        System.out.println(evenNumbersAverage);
     }
 }
